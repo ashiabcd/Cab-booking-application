e25a826e8135dcc4d05721ebcfdec29dd77efee1
Ashi Sinha <56842767+ashiabcd@users.noreply.github.com>
2020-08-12 09:35:08 +0530

    Add files via upload

---
+++ src/Logins/Drivers.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logins;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author PRANAVI MARRIPUDI
+ */
+/*public class Drivers implements Serializable{
+    static ArrayList<Drivers> drr= new ArrayList<>();
+    String name;
+    String id;
+    String car_id;
+    double rating;
+    int present_loc;
+    int end_loc;
+    boolean is_driving;
+    Drivers(){};
+    Drivers(String name, String id, String car_id, double rating, int pres_loc, int end_loc, boolean is_driving){
+        this.name= name;
+        this.id= id;
+        this.car_id= car_id;
+        this.rating= rating;
+        present_loc= pres_loc;
+        this.end_loc= end_loc;
+        drr.add(this);
+       String f="data2";
+       try{
+       FileOutputStream fos= new FileOutputStream(f);
+       ObjectOutputStream oos=new ObjectOutputStream(fos);
+       oos.writeObject(drr);
+      // System.out.println("In constructor :"+ drr);
+       }
+       
+       catch(Exception ae){ }
+    }
+    }*/
+    
+
